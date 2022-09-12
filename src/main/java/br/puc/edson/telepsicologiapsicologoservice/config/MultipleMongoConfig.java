3f615103a91c8a28515893b21a7e45d63d15b437
Edson Galdino da Silva Alves <edson1galdino@gmail.com>
2022-09-12 12:51:39 -0300

    Removendo import não utilizado

--- src/main/java/br/puc/edson/telepsicologiapsicologoservice/config/MultipleMongoConfig.java
+++ src/main/java/br/puc/edson/telepsicologiapsicologoservice/config/MultipleMongoConfig.java
@@ -1,6 +1,5 @@
 package br.puc.edson.telepsicologiapsicologoservice.config;
 
-import com.mongodb.client.MongoClient;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
